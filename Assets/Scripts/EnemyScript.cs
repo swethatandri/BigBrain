0cf3bf415a6e88f92fb0a7cd7021da33eb5a9898
swethatandri <65615296+swethatandri@users.noreply.github.com>
2021-02-20 15:19:29 -0600

    Add files via upload

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject brain;
+    public float speed = 3f;
+    private float enemyBound = -5;
+    public GameObject player;
+    public PlayerController pc;
+    
+
+  
+
+
+
+    public int referenceNumber;
+
+
+    void Start()
+    {
+        
+        player = GameObject.Find("Player");
+        pc = player.GetComponent<PlayerController>();
+
+        referenceNumber = 10;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        transform.Translate(Vector3.back * speed * Time.deltaTime); 
+        destoyEnemy();
+
+        UpdateSpeed();
+
+        if(pc.gameOver == true){
+            speed = 0;
+        } 
+
+        
+    }
+
+
+    void destoyEnemy(){
+
+        if(transform.position.z < enemyBound){
+
+            Destroy(gameObject);
+        }
+
+    }
+
+   
+
+    void UpdateSpeed(){
+
+
+        int score = GameObject.Find("Player").GetComponent<PlayerController>().score;
+
+        if(score >= referenceNumber){
+
+            speed *= 1.2f;
+
+            referenceNumber += 10;
+            
+
+
+        }
+
+    }
+
+   
+}
+
---
+++ Assets/Scripts/PillMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillMovement : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    public float pillspeed = 5f;
+
+    public PlayerController pc; 
+    
+
+    void Start()
+    {
+
+        pc = GameObject.Find("Player").GetComponent<PlayerController>();
+        StartCoroutine(pillTime());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        destroyPill();
+
+        transform.Translate(Vector3.back * pillspeed *Time.deltaTime);
+
+        if(pc.gameOver == true){
+            pillspeed = 0;
+        }
+
+
+        
+    }
+
+    void destroyPill(){
+
+        if(transform.position.z < -5){
+
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    IEnumerator pillTime(int seconds = 4){
+
+        if(pc.timedGameOver == false){
+
+
+        while(seconds > 0){
+
+            yield return new WaitForSeconds(1);
+            seconds--;
+
+            
+        }
+
+        Destroy(gameObject);
+        }
+
+
+    }
+}
